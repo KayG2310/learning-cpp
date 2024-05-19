dbba1da4dc5711e3afae7dbc13c54e05bbb720f1
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-05-19 23:11:38 +0530

    Create monksandmarks.cpp

---
+++ monksandmarks.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    map<int, set<string>>m;
+    int N;
+    cin>>N;
+    for(int i=0; i<N; i++){
+        string name;
+        int marks;
+        cin>>name>>marks;
+        m[marks].insert(name);
+    }
+    auto it = m.end();
+    it--;
+    while(true){
+        auto &name = (*it).second;
+        int marks = (*it).first;
+        
+        
+        for(auto &val:name){
+            cout<<val<<" "<<marks <<endl;
+            
+        }
+        if(it== m.begin()){
+            break;
+        } else{
+            it--;
+        }
+    }
+}
