0a2d166c7503382c9a56d47e9e0c21c69f62dca7
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-05-21 01:23:10 +0530

    Update lettercount.cpp

--- lettercount.cpp
+++ lettercount.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() {
+    string s;
+    cin>>s;
+    map<char,int>m;
+    for(int i=0; i<s.length();i++){
+        m[s[i]]-=1;
+    }
+    map<int, set<char>>m2;
+    for(auto it=m.begin(); it!=m.end();it++){
+        char character = (*it).first;
+        int frequency = (*it).second;
+        m2[frequency].insert(character);
+    }
+    
+    auto it2=m2.begin();
+    auto output2 = (*it2).second;
+    for(auto &val: output2){
+        cout<<val;
+        break;
+    }
+    
+}
