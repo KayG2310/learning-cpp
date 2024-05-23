4f32633f21d3af0466f74adf9b51ae1164cd5cdf
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-05-23 22:30:34 +0530

    Update binary search.cpp

--- binary search.cpp
+++ binary search.cpp
@@ -2,11 +2,37 @@
 using namespace std;
 
 int main(){
-    vector<int> v = {4, 8, 15, 16, 27, 34, 42,46,50);
-    int num, length, mid, index, check;
+    vector<int> v = {4, 8, 15, 16, 27, 34, 42,46,50};
+    int num, end, mid, index, check,start;
     num = 42;
-    length = v.size();
-    mid = length/2;
+    end = v.size();
+    start = 0;
     index = -1;
-    check = mid -1;
+    int count = 0;
+    /*loop runs
+    start = 0, end = 9, mid = 4, check = 3
+    16 < 42
+    start = 4, end = 9, mid = 6, check = 5 {27,34,42,46,50}
+    42 == 42*/
+    while(index==-1){
+        mid = (start+end)/2;
+        check = mid -1;
+        count++;
+        if(v[check]==num){
+            index = check;
+            break;
+        } else if(v[check]> num){
+            // we have to discard right side
+            end = check-1;
+        } else if (v[check]<num){
+            // we discard the left side
+            start = check+1;
+        } else{
+            continue;
+        }
+        if(end==start){
+            break;
+        }
+    }
+    cout<<"found at index: "<< index<<endl<<"steps taken: "<<count;
 }
