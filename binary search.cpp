6b451bdd8b9bf44a9a92f83c7de0cf1efc3e3443
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-05-23 21:44:53 +0530

    Update binary search.cpp

--- binary search.cpp
+++ binary search.cpp
@@ -1,1 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    vector<int> v = {4, 8, 15, 16, 27, 34, 42,46,50);
+    int num, length, mid, index, check;
+    num = 42;
+    length = v.size();
+    mid = length/2;
+    index = -1;
+    check = mid -1;
+}
