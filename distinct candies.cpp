0741d8d577e10ef0805ca785f7ddbd306151c907
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-05-25 03:04:46 +0530

    Update distinct candies.cpp

--- distinct candies.cpp
+++ distinct candies.cpp
@@ -1,1 +1,27 @@
+// leetcode question
+#include<bits/stdc++.h>
+using namespace std;
+
+int distributeCandies(vector<int> candies) {
+    map<int, int> m;
+    for(int i = 0; i<candies.size();i++){
+        int k = candies[i];
+        m[k]++;
+    }
+    int size= m.size();
+    int caneat = candies.size()/2;
+    int types = 0;
+    if(caneat>=size){
+        types = size;
+    }
+    else {
+        types = caneat;
+    }
+    return types;
+}
+
+int main(){
+    vector<int> v = {1,1,2,2,3,3};
+   cout<<distributeCandies(v);
+}
 
