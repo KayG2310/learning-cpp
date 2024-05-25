5427badfcb1cfb399fb1cc28376b6730f2f84ffb
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-05-26 02:24:09 +0530

    Update update ith bit.cpp

--- update ith bit.cpp
+++ update ith bit.cpp
@@ -4,8 +4,17 @@ using namespace std;
 int main() {
     int x,i;
     cin>>x>>i;
+    int v;
+    cin>>v;
     // this time we need to do an OR operation
-    int mask = (1<<i);
-    x = x | mask;
+    // combo of set & clear
+    
+    int mask = ~(1<<i);
+    x = x & mask; // clearing the ith bit
+    
+    int mask2 = (v<<i);
+    // v could be zero and one
+    x = x|mask2 ;
     cout<<x;
+    
 }
