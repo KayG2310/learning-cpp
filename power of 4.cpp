01c7e209012582afb5aae558f33107f8aaa38715
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-05-26 18:01:00 +0530

    Update power of 4.cpp

--- power of 4.cpp
+++ power of 4.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool check4(int N){
+    int mask;
+    int position = log(N)/log(4); // gives 4-> 1, 16->2
+    mask = N>>(2*position);
+    
+    if(mask==1){
+        if(position==0){
+            return false;}
+            
+        return true;
+        
+    }else{return false;}
+}
+
+
 int main() {
     // to check if a number inputted is a power of 4 or not
     // power of 4 looks like 1 on even indices and others zero
@@ -10,13 +25,7 @@ int main() {
     int N;
     cin>>N
     ;
-    int mask;
-    int position = log(N)/log(4); // gives 4-> 1, 16->2
-    mask = N>>(2*position);
-    if(mask==1){
-        cout<<"yes";
-        
-    }else{cout<<"no";}
+    cout<<check4(N);
     
     
 }
