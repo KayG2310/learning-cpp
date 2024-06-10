2319b5a516d237ffa2366e22bf3333b58fa66f53
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-06-11 00:39:09 +0530

    Update coprime divisor.cpp

--- coprime divisor.cpp
+++ coprime divisor.cpp
@@ -19,5 +19,41 @@ int solve(int a,int b){
         n++;
     }
     return a/n;
-} */
+} */ 
+// above is a good solution
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int s, int k){
+    if(s<k){
+        swap(s,k);
+    }
+    if(k==0){
+        return s;
+    }
+    else{
+        return gcd(k,s%k);
+    }
+}
+int solve(int a,int b){
+    
+    vector<int>divisors;
+    for(int i=pow(a,0.5); i>=1;i--){
+        if(a%i==0){
+            divisors.push_back(i);
+            divisors.push_back(a/i);
+        }
+    }
+    // we have divisors
+    sort(divisors.begin(), divisors.end());
+    auto it = divisors.end();
+    it--;
+    while(it!=divisors.begin()){
+        if(gcd(*it, b)==1){
+            return *it;
+        }
+        it--;
+    }
+    return -1;
+}
+
 
