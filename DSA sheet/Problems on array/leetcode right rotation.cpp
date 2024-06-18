b854a2aed35091d68ab749dfd9ffd1e3728b418e
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-06-18 19:47:39 +0530

    opt

--- DSA sheet/Problems on array/leetcode right rotation.cpp
+++ DSA sheet/Problems on array/leetcode right rotation.cpp
@@ -31,3 +31,16 @@ public:
 
 
 // optimal approach
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n; 
+        if (k == 0 || n == 1) return; 
+        reverse(nums.begin() + n - k, nums.end());
+        
+        reverse(nums.begin(), nums.begin() + n - k);
+        
+        reverse(nums.begin(), nums.end());
+    }
+};
