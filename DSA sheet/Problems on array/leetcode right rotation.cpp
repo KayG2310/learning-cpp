6469b094fa3ad56edd5eb387577b4dbbb02d2ae1
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-06-18 19:10:06 +0530

    added brute force method

--- DSA sheet/Problems on array/leetcode right rotation.cpp
+++ DSA sheet/Problems on array/leetcode right rotation.cpp
@@ -1,1 +1,33 @@
 // brute force 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        int size2 = nums.size(); //4
+
+        k = k%size2; // k = 2
+        if(size2==1 || k==0) return;
+        int temp[k];
+        for(int i=size2-k; i<size2;i++){
+            temp[i+k-size2] = nums[i];
+        }
+        // 2<=i<4, i = 2,3
+        // i = 2, temp[2+2-4=0] = nums[2] => temp = {3}
+        // i = 3, temp[3+2-4=1] = nums[3] => temp = {3,99}
+        // now our task is to input -1,-100 correctly
+        
+        for (int i = size2 - 1; i >= k; i--) {
+            nums[i] = nums[i - k];
+        }
+
+        for(int i=0; i<k; i++){
+            nums[i] = temp[i];
+        }
+    }
+};
+
+
+
+
+
+// optimal approach
