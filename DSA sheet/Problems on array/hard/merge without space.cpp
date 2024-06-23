3167a3f5b7033462550e602ab739fa3bceaf7eb9
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-06-24 02:05:13 +0530

    added gap method imp

--- DSA sheet/Problems on array/hard/merge without space.cpp
+++ DSA sheet/Problems on array/hard/merge without space.cpp
@@ -39,3 +39,39 @@ int main() {
     sort(nums1.begin(), nums1.end());
     sort(nums2.begin(), nums2.end());
 }
+
+// optimal 2 -->
+void merge(long long arr1[], long long arr2[], int n, int m){
+    int len = n+m;
+    int gap = (len/2)+(len%2);
+    while(gap>0){
+        int left = 0;
+        int right = left+gap;
+        while(right<len){
+            if(left<n && right>=n){
+                // left in 1 right in 2
+                if(arr1[left]>arr2[right-n]){
+                    swap(arr1[left], arr2[right-n]);
+                left++;
+                right++;
+                }
+            }
+            else if(left>=n && right<len){
+                // both in arr[2]
+                if(arr2[left-n]>arr2[right-n]){
+                    swap(arr2[left-n], arr2[right-n]);
+                }
+                left++;
+                right++;
+            }
+            else {
+                if(arr1[left]>arr2[right]){
+                    swap(arr1[left], arr1[right]);
+                }
+                left++;
+                right++;
+            }
+        }
+        gap = (gap/2)+gap%2;
+    }
+}
