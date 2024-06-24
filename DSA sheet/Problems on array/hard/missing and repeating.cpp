ef269b514d2f6effd09392d27e013d2cad56ce71
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-06-24 18:12:42 +0530

    added xor approach

--- DSA sheet/Problems on array/hard/missing and repeating.cpp
+++ DSA sheet/Problems on array/hard/missing and repeating.cpp
@@ -69,5 +69,46 @@ int main() {
     
     cout<<"repeating: "<<(xminusy+xplusy)/2<<endl;
     cout<<"missing: "<<(xplusy-xminusy)/2;
+
+}
+
+
+// optimal ---> 2 using xor
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {4,2,2,3,6,1};
+    int n = sizeof(arr)/sizeof(int);
     
+    vector<int>ans;
+    int xr = 0;
+    for(int i=0; i<n; i++){
+        xr = xr ^ arr[i];
+        xr = xr ^ (i+1);
+    }
+    int bitno = 0;
+    while(1){
+        if(xr & (1<<bitno) !=0){
+            break;
+        }
+        bitno++;
+    }
+    
+    // now we have to segregate numbers
+    int zero = 0;
+    int one = 0;
+    for(int i=0; i<n; i++){
+        if((arr[i] & (1<<bitno)) != 0){
+            one = one ^ arr[i];
+            one = one ^ (i+1);
+        }
+        else {
+            zero = zero ^ arr[i];
+            zero = zero ^ (i+1);
+        }
+        
+        // if it doesn't matter the order of repeating and missing
+        // no need to check which out of zero or one was which
+    }
 }
