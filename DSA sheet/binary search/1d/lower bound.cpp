0cf69a8d205711679ef19b80eb82b22199ad1d79
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-06-26 15:06:23 +0530

    using binary

---
+++ DSA sheet/binary search/1d/lower bound.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[] = {3,4,6,7,9,12,14,16,17};
+    int n = sizeof(arr)/sizeof(int);
+    
+    int target = 13;
+    int low=0, high = n-1;
+    int mid = 0;
+    int ans = n; // assuming it DNE
+   
+    while(low<=high){
+        mid = (low+high)/2;
+        if(arr[mid]>=target){
+            ans = min(ans, mid);
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+    cout<<ans;
+}
