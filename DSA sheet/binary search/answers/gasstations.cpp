db4c24e066f93705ede73b0a9ba9342f1326096b
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-07-04 19:10:46 +0530

    Update gasstations.cpp

--- DSA sheet/binary search/answers/gasstations.cpp
+++ DSA sheet/binary search/answers/gasstations.cpp
@@ -28,3 +28,33 @@ class Solution {
     }
 };
 
+
+// using heaps or priority queues
+class Solution {
+  public:
+    double findSmallestMaxDist(vector<int> &stations, int k) {
+        int n = stations.size();
+        vector<int> howmany(n-1,0);
+        priority_queue<pair<long double,int>>pq;
+        
+        for(int i=0; i<n-1; i++){
+            pq.push({stations[i+1]-stations[i],i});
+        }
+        for(int gas=1; gas<=k; gas++){
+            auto tp = pq.top();
+            pq.pop();
+            int secind = tp.second;
+            howmany[secind]++;
+            long double initial = tp.first;
+            long double final = initial/(long double)(howmany[secind]+1);
+            pq.push({final, secind});
+            
+        }
+        
+       
+        return pq.top().first;
+    }
+}
+
+// binary search
+//in png
