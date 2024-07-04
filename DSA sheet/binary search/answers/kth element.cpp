ecd9478e12f181591567855c2162d15023be1c09
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-07-05 00:38:29 +0530

    Update kth element.cpp

--- DSA sheet/binary search/answers/kth element.cpp
+++ DSA sheet/binary search/answers/kth element.cpp
@@ -1,3 +1,4 @@
+// tc = O(k) loop will run till whenever k is found
 class Solution{
     public:
     int kthElement(int nums1[], int nums2[], int n, int m, int k)
@@ -27,3 +28,30 @@ class Solution{
         }
     }
 };
+
+//log(min(n1,n2))
+double median(vector<int> &a, vector<int>&b,n1,n2,k){
+    
+    if(n1>n2) return median(b,a,n2,n1,k);
+    int low = max(0,k-n2), high = min(n1,k);
+    int left = k;
+    int n = n1+n2;
+    while(low<=high){
+        int mid1 = (low+high)/2;
+        int mid2 = left - mid1;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if(mid1<n1) r1 = a[mid1];
+        if(mid2<n2) r2 = b[mid2];
+        if(mid1-1>=0) l1 = a[mid1-1];
+        if(mid2-1>=0) l2 = a[mid2-1];
+        if(l1<=r2 && l2<=r1){
+                return max(l1,l2);
+        }
+        else if(l1>r2){
+            high = mid1-1;
+        }
+        else low = mid1+1;
+    }
+    return 0;
+}
