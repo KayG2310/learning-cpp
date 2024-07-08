2550afac998f07848a6db2c82dcbd477b20baa34
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-07-09 01:35:41 +0530

    sanat's optimal code

--- DSA sheet/strings/basic/check rotated.cpp
+++ DSA sheet/strings/basic/check rotated.cpp
@@ -28,3 +28,47 @@ public:
     }
 };
 
+// optimal, linear
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+    int i=0,j=0;
+    int n=s.length();
+    int matched=0;
+    int k=0;
+    while(i<n && j<2*n){
+        //cout<<i<<" "<<j<<" "<<matched<<" "<<k<<endl;
+        if(s[i]==goal[j%n]){
+            i++;
+            j++;
+            matched++;
+            if(matched==1) k=j;
+        }
+        else{
+            if(i==0){
+                j++;
+            }
+            else{
+                j=k;
+            }
+            i=0;
+            //j=j+k+1;
+            matched=0;
+        }
+    }
+    if(matched==n) return true;
+    else return false;
+}
+};
+
+//s=bbbacddceeb
+//g=ceebbbbacdd
+
+//i=0,j=0,n=11,m=0,k=0 ---INITIAL
+//i=0,j=1,m=0
+//i=0,j=2,m=0
+//i=0,j=3,m=0
+//i=1,j=4,m=1,k=4
+//i=2,j=5,m=2,k=4
+//i=3,j=6,m=3,k=4
+//i=0,j=4,m=0,k=4
