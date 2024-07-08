764d91843359aa18bbc5872656795dae1267cd53
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-07-08 23:12:26 +0530

    Update isomorphic.cpp

--- DSA sheet/strings/basic/isomorphic.cpp
+++ DSA sheet/strings/basic/isomorphic.cpp
@@ -28,3 +28,35 @@ public:
         //3n + 2(nlogn)
     }
 };
+
+
+// using string replace --> does not work
+// two maps
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char>stot,ttos;
+        for(int i=0; i<s.length(); i++){
+            if(stot.find(s[i])==stot.end()){
+                stot[s[i]] = t[i];
+            }
+            else{
+                if(stot[s[i]]==t[i]) continue;
+                else return false;
+            }
+        }
+        for(int i=0; i<t.length(); i++){
+            if(ttos.find(t[i])==ttos.end()){
+                ttos[t[i]] = s[i];
+            }
+            else{
+                if(ttos[t[i]]==s[i]) continue;
+                else return false;
+            }
+        }
+        return true;
+
+    }
+};
+//O(N), sc = O(2n)
+
