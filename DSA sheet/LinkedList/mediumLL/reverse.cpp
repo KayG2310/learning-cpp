f9ebd615d3ae965894a9aa1847df9d2698be24c2
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-07-12 19:49:45 +0530

    recursion imp

---
+++ DSA sheet/LinkedList/mediumLL/reverse.cpp
@@ -0,0 +1,50 @@
+// using stacks - tc = 2n, sc = n
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        stack<int>els;
+        ListNode* temp = head;
+        while(temp != nullptr){
+            els.push(temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        while(temp != nullptr){
+            temp->val = els.top();
+            els.pop();
+temp = temp->next;
+        }
+        return head;
+    }
+};
+
+// in  place to optimise this, play around with the links, tc=n, sc=1
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
+        while(temp != nullptr){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+
+    }
+};
+
+// recursive approach
+Node* reverse_recursion(Node* head){
+    if(head==NULL || head->next == NULL){
+        return head;
+    }
+    Node* newhead = reverse_recursion(head->next);
+    Node* front = head->next;
+    front->next = head;
+    head->next = NULL;
+    return newhead;
+}
+
