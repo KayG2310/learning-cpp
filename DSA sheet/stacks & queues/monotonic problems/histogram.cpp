efad2477ab17d2f8734a8cd5897aebde28226229
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-08-01 00:00:07 +0530

    optimal

--- DSA sheet/stacks & queues/monotonic problems/histogram.cpp
+++ DSA sheet/stacks & queues/monotonic problems/histogram.cpp
@@ -55,3 +55,27 @@ public:
         return area;
     }
 };
+
+// very optimal code 
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        // brute force
+        int n = heights.size();
+        stack<int>st;
+        int area = 0;
+        for(int i=0; i<=n; i++){
+            while(!st.empty() && (i==n || heights[st.top()] >= heights[i])){
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = i - st.top() - 1;
+                area = max(area, height*width);
+            }
+            
+            st.push(i);
+        }
+        return area;
+    }
+};
