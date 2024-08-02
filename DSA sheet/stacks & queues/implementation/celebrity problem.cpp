b3c44f80026960ed044b53a95d925b2fc6293255
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-08-03 02:08:26 +0530

    stack solution

--- DSA sheet/stacks & queues/implementation/celebrity problem.cpp
+++ DSA sheet/stacks & queues/implementation/celebrity problem.cpp
@@ -58,3 +58,63 @@ class Solution {
 };
 
 // using stacks (same logic) keep popping and appending two
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& mat) {
+        // code here
+        stack<int> st;
+        int n=mat.size();
+        for(int i=0;i<n;i++) st.push(i);
+        while(st.size()>1){
+            int a = st.top();
+            st.pop();
+            int b = st.top();
+            st.pop();
+            if(mat[a][b]==1){ //a knows b => a cannot be celebrity
+                st.push(b);
+            }
+            else{ //a doesnt know b => b cannot be celebrity
+                st.push(a);
+            }
+        }
+        int potential = st.top(); //potential candidate
+        st.pop();
+        for(int i=0;i<n;i++){
+            if(mat[potential][i]!=0) return -1; //check the entire row
+            if(i!=potential && mat[i][potential]!=1) return -1; //check the entire col
+        }
+        return potential;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > M(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> M[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.celebrity(M) << endl;
+    }
+}
+
+// } Driver Code Ends
