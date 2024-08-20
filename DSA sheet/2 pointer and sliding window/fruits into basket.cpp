ca0cbab4c8b26d56674d9158a86c1f0550e710ab
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-08-20 15:51:19 +0530

    Update fruits into basket.cpp

--- DSA sheet/2 pointer and sliding window/fruits into basket.cpp
+++ DSA sheet/2 pointer and sliding window/fruits into basket.cpp
@@ -21,3 +21,49 @@ class Solution {
         return maxlen;
     }
 };
+
+// better using two pointer, tc = 2n, sc = constant
+class Solution {
+  public:
+    int totalFruits(vector<int> &arr) {
+        int maxlen = 0;
+        int l=0, r=0;
+        unordered_map<int,int>track;
+        
+        while(r<arr.size()){
+            track[arr[r]]++;
+            while(track.size()>2){
+                track[arr[l]]--;
+                if(track[arr[l]]==0) track.erase(arr[l]);
+                l++;
+            }
+            maxlen = max(maxlen, r-l+1);
+            r++;
+            
+        }
+        return maxlen;
+    }
+};
+
+// optimal approach ---> tc = n, sc = constant
+class Solution {
+  public:
+    int totalFruits(vector<int> &arr) {
+        int maxlen = 0;
+        int l=0, r=0;
+        unordered_map<int,int>track;
+        
+        while(r<arr.size()){
+            track[arr[r]]++;
+            if(track.size()>2){
+                track[arr[l]]--;
+                if(track[arr[l]]==0) track.erase(arr[l]);
+                l++;
+            }
+            if(track.size()<=2)maxlen = max(maxlen, r-l+1);
+            r++;
+            
+        }
+        return maxlen;
+    }
+};
