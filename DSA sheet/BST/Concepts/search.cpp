205d32383dfe4105a7ba9060268054c41e0e7af8
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-08-30 00:39:09 +0530

    Create search.cpp

---
+++ DSA sheet/BST/Concepts/search.cpp
@@ -0,0 +1,50 @@
+// recursive approach = logn, sc = logn
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL) return root;
+        if(root->val == val) return root;
+        if(root->val > val) return searchBST(root->left, val);
+        return searchBST(root->right, val);
+    }
+};
+
+
+
+// iterative approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root != NULL && root->val != val){
+            if(root->val > val){
+                root = root->left;
+            }
+            else{
+                root = root->right;
+            }
+        }
+        return root;
+    }
+};
