2b0bcc0bcbad3858af743a788d1a8a75c08fbec8
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-10-06 00:18:00 +0530

    2 approaches

--- DSA sheet/BST/Practice/bst from preorder.cpp
+++ DSA sheet/BST/Practice/bst from preorder.cpp
@@ -34,3 +34,36 @@ public:
 
     }
 };
+
+
+
+
+// striver soln, tc = 3n, sc = 1
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    
+public:
+    TreeNode* build(vector<int>& a, int &i, int bound){
+        if(a.size() == i || a[i]>bound) return NULL;
+        // we have to stay below the bound
+        TreeNode* root = new TreeNode(a[i++]);
+        root->left = build(a, i, root->val);
+        root->right = build(a, i, bound);
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i = 0;
+        return build(preorder, i, INT_MAX);
+    }
+};
