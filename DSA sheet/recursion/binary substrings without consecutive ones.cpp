66c86e417f53fdd9594e7fe2d6b836e2a20b09a8
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-10-08 19:49:44 +0530

    Update binary substrings without consecutive ones.cpp

--- DSA sheet/recursion/binary substrings without consecutive ones.cpp
+++ DSA sheet/recursion/binary substrings without consecutive ones.cpp
@@ -16,3 +16,23 @@ public:
 
 // it works with iterative approach much better
 // recursive 2 ---> method in copy
+
+
+// iterative - n
+class Solution{
+public:
+	// #define ll long long
+	const int mod = 1e9+7;
+	ll countStrings(int n) {
+	    ll oneend = 1, zeroend = 1;
+	    if(n==1) return 2;
+	    int i=2;
+	    while(i<=n){
+	        ll sum = (oneend+zeroend)%mod;
+	        oneend = zeroend;
+	        zeroend = sum;
+	        i++;
+	    }
+	    return (oneend+zeroend)%mod;
+	}
+};
