c6253ebadd48b50684f3b3943a65732a83db5c9f
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-10-11 11:33:34 +0530

    Update combo sum 2.cpp

--- DSA sheet/recursion/combo sum 2.cpp
+++ DSA sheet/recursion/combo sum 2.cpp
@@ -28,3 +28,30 @@ public:
         return ans;
     }
 };
+
+// tc = 2^t*k
+class Solution {
+public:
+    void print(int index, int target, vector<int>&ds, vector<int>&candidates, vector<vector<int>>&ans1){
+        if(target == 0){
+            ans1.push_back(ds);
+            return;
+        }
+        for(int i=index; i<candidates.size(); i++){
+            if(i>index && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+            ds.push_back(candidates[i]);
+            print(i+1, target-candidates[i], ds, candidates, ans1);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        // if(accumulate(candidates.begin(), candidates.end(),0)==target) return {candidates};
+        sort(candidates.begin(), candidates.end());
+        // set<vector<int>>ans1;
+        vector<int>ds;
+        vector<vector<int>>ans;
+        print(0, target, ds, candidates, ans);
+        return ans;
+    }
+};
