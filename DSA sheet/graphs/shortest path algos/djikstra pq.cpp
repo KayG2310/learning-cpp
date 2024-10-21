0cea400aff54fc37c0a0e770661fc14d35a337f4
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-10-22 02:48:36 +0530

    Update djikstra pq.cpp

--- DSA sheet/graphs/shortest path algos/djikstra pq.cpp
+++ DSA sheet/graphs/shortest path algos/djikstra pq.cpp
@@ -32,3 +32,43 @@ class Solution {
         
     }
 };
+
+
+
+// using set
+class Solution {
+  public:
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex src.
+    vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src) {
+        // Code here
+        int n = adj.size();
+        vector<int>dist(n, 1e9);
+        dist[src] = 0;
+        set<pair<int, int>>st;
+        st.insert({0, src});
+        while(!st.empty()){
+            auto it = *(st.begin());
+            int dis = it.first;
+            int node = it.second;
+            st.erase(it);
+            
+            // go to neighbours
+            for(auto it: adj[node]){
+                int ew = it.second;
+                int no = it.first;
+                if(dis+ew < dist[no]){
+                    if(dist[no] != 1e9) st.erase({dist[no], no});
+                    dist[no] = dis+ew;
+                    st.insert({dist[no], no});
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(dist[i] == 1e9) dist[i] = -1;
+        }
+        return dist;
+        
+        
+    }
+};
