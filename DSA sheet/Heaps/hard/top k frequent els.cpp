def1cab521f9d36529bbeee5b2ae26d9483fbb34
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-12-15 01:09:00 +0530

    Create top k frequent els.cpp

---
+++ DSA sheet/Heaps/hard/top k frequent els.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int>m;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            m[nums[i]]++;
+        }
+        priority_queue<pair<int,int>>pq;
+        for(auto &it : m){
+            pq.push({it.second, it.first});
+        }
+        vector<int>ans;
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+}; 
+
+// tc = nlogn+k
+// sc = O(n) 
