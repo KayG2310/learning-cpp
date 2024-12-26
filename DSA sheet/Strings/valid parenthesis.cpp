2535b4cf84b6b2c67d2447f9ced8f1ffbd37b627
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-12-26 14:32:40 +0530

    Update valid parenthesis.cpp

--- DSA sheet/Strings/valid parenthesis.cpp
+++ DSA sheet/Strings/valid parenthesis.cpp
@@ -15,3 +15,22 @@ public:
 }; 
 
 // tc = n, sc = n
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        // stack<char>st;
+        int open = 0;
+        int other = 0;
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            if(s[i]=='(') open++;
+            else{
+                if(open>0) open--;
+                else other++;
+            }
+        }
+        return open+other;
+    }
+};
+// tc = n, sc = 1
