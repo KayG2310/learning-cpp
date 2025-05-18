66c8881675b11419bc74d401bcbd3a62eb4335ad
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-05-18 17:00:14 +0530

    Create post to in.cpp

---
+++ DSA sheet/stacks & queues/prefix postfix infix/striver/post to in.cpp
@@ -0,0 +1,6 @@
+/*
+step1 : whenever you encounter a operand, push it in the stack
+step2 : if it is an operator, pop out the 2 elements from stack for eg if stack was [A B] and operator was - do it like (A-B) and push this
+
+
+*/
