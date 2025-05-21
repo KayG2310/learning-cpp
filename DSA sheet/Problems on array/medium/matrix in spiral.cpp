68b97fe0a14fd76a7a73dd8a1478ed6c49537412
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-05-21 19:56:38 +0530

    brute hi hai

--- DSA sheet/Problems on array/medium/matrix in spiral.cpp
+++ DSA sheet/Problems on array/medium/matrix in spiral.cpp
@@ -1,24 +1,34 @@
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int r = matrix.size();
+        int c = matrix[0].size();
+        vector<int>ans;
+        int left = 0, right = c-1, top = 0, bottom = r-1;
+        while(top<=bottom && left<=right){
+            for(int i=left; i<=right; i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int i=top; i<=bottom; i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
 
-int main(){
-    vector<vector<int>> v= {{1,2,3,4},
-    {5,6,7,8},
-    {9,10,11,12},
-    {13,14,15,16}};
-    for(int i=0; i<v.size(); i++){
-        for(int j=i+1; j< v.size(); j++){
-            if(i != j){
-                swap(v[i][j], v[j][i]);
+                for(int i=right; i>=left; i--){
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+
+                for(int i=bottom; i>=top; i--){
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
             }
         }
+        return ans;
     }
-    for(int i=0; i<v.size(); i++){
-        reverse(v[i].begin(), v[i].end());
-        for(int j=0; j< v.size(); j++){
-            cout<<v[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    
-}
+};
