daf1024d4a700e6449fde037c9147e81154a74c9
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-12-20 22:08:24 +0530

    2 approaches
    Add comments explaining two approaches to merge two BSTs.

---
+++ DSA sheet/BST/Practice/Merge 2 BST.cpp
@@ -0,0 +1,2 @@
+// approach 1 --> inorder traversal of both + merge two sorted arrays --> from sorted form a sorted array 
+// approach 2 --> flatten a bst into a linkedlist you end up saving space for sorted arrays.
