8e9fb8245a43578226f38b4e1c86736a9ecbdd5c
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-12-29 00:08:26 +0530

    code was wrong earlier

--- DSA sheet/DP/MCM or Partition/partition array max sum.cpp
+++ DSA sheet/DP/MCM or Partition/partition array max sum.cpp
@@ -1,29 +1,23 @@
 // recursive code
 class Solution {
 private:
-    bool pal(int i, int j, string &str){
-        while(i<=j){
-            if(str[i] != str[j]) return false;
-            i++;
-            j--;
+    int f(int ind, int n, int k, vector<int>&arr){
+        if(ind==n) return 0;
+        int len = 0;
+        int maxi = INT_MIN;
+        int maxans = INT_MIN;
+        for(int j=ind; j<min(n, ind+k); j++){
+            len++;
+            maxi = max(maxi, arr[j]);
+            int sum = len*maxi + f(j+1, n, k, arr, dp);
+            maxans = max(maxans, sum);
         }
-        return true;
+        return maxans;
     }
 public:
-    int minCut(string s) {
-        int n = s.length();
-        vector<int>dp(n+1, 0);
-        for(int i=n-1; i>=0; i--){
-            int mincost = INT_MAX;
-            for(int j=i; j<n; j++){
-                if(pal(i, j, s)){
-                    int cost = 1 + dp[j+1];
-                    mincost = min(mincost, cost);
-                }
-            }
-            dp[i] = mincost;
-        }
-        return dp[0]-1;
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        return f(0, n, k, arr);
     }
 };
 
