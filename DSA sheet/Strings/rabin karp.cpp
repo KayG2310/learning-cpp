cb31c981169c843f9b4db12dcc20b9df1fce51da
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2026-01-03 23:31:27 +0530

    string hashing v v imp

--- DSA sheet/Strings/rabin karp.cpp
+++ DSA sheet/Strings/rabin karp.cpp
@@ -1,45 +1,18 @@
-class Solution {
-private:
-    int BASE = 1000000;
-public:
-    int repeatedStringMatch(string A, string B) {
-        if(A == B) return 1;
-        int count = 1;
-        string source = A;
-        while(source.size() < B.size()){
-            count++;
-            source+=A;
-        }
-        if(source == B) return count;
-        if(Rabin_Karp(source,B) != -1) return count;
-        if(Rabin_Karp(source+A,B) != -1) return count+1;
-        return -1;
-    }
-    int Rabin_Karp(string source, string target){
-        if(source == "" or target == "") return -1;
-        int m = target.size();
-        int power = 1;
-        for(int i = 0;i<m;i++){
-            power = (power*31)%BASE;
-        }
-        int targetCode = 0;
-        for(int i = 0;i<m;i++){
-            targetCode = (targetCode*31+target[i])%BASE;
-        }
-        int hashCode = 0;
-        for(int i = 0;i<source.size();i++){
-            hashCode = (hashCode*31 + source[i])%BASE;
-            if(i<m-1) continue;
-            if(i>=m){
-                hashCode = (hashCode-source[i-m]*power)%BASE;
-            }
-            if(hashCode<0)
-                hashCode+=BASE;
-            if(hashCode == targetCode){
-                if(source.substr(i-m+1,m) == target)
-                    return i-m+1;
-            }
-        }
-        return -1;
-    }
-};
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        prefix = 0
+        suffix = 0
+        base = 29
+        last_index = 0 # -1
+        power = 1
+        mod = 10**9 + 7
+        for i, c in enumerate(s) :
+            char = (ord(c) - ord('a') + 1)
+            prefix = (prefix * base) % mod
+            prefix = (prefix + char) % mod
+            suffix = (suffix + char * power) % mod
+            power = (power * base) % mod
+            if prefix == suffix:
+                last_index = i
+        suffix = s[last_index + 1:]
+        return suffix[::-1] + s       
