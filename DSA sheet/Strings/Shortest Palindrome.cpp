38e561a8722fbabe52835a149bdf46d748bdeedb
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2026-01-03 22:43:18 +0530

    brute force

---
+++ DSA sheet/Strings/Shortest Palindrome.cpp
@@ -0,0 +1,5 @@
+// brute force solution 
+/*
+steps --> check if string from 0 to n-1, 0 to n-2 and so on is palindrome. Then add the remaining portion reversed in the starting 
+of the original string
+*/
