fe90f1f3b94f43494688c6e833332ec9cd5773d0
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2026-03-12 19:17:34 +0530

    Remove comments

--- DSA sheet/BST/Practice/recover.cpp
+++ DSA sheet/BST/Practice/recover.cpp
@@ -1,25 +1,9 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 private:
-    TreeNode* first;
-    TreeNode* last;
-    TreeNode* middle;
-    TreeNode* prev;
-
+    TreeNode* first, last, middle, prev;
     void inorder(TreeNode* root){
         if(root == NULL) return;
         inorder(root->left);
-
         if(prev != NULL && (root->val < prev->val)){
             // first violation
             if(first == NULL){
