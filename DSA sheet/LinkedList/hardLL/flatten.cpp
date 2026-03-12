15cf70106e3b32127de3092825f7251dda383d79
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2026-03-12 18:06:32 +0530

    cleaned

--- DSA sheet/LinkedList/hardLL/flatten.cpp
+++ DSA sheet/LinkedList/hardLL/flatten.cpp
@@ -1,25 +1,8 @@
 // extreme naive solution -> traverse through all elements in a LL with its child nodes and put them in an array
 //once you have the array, sort it and make a vertical linked list
-/*
-pseudocode 
-arr = []
-temp = head;
-while(temp != NULL){
-  t2 = temp;
-  add temp
-  while(t2 != NULL){
-    add t2->data to arr
-    t2 = t2->bottom
-  }
-  temp = next;
-}
-sort(arr);
-head = convert(arr) and convert function can be created one by one
-tc = n*m*2 + (n*m)log(n*m);
-*/
+
 // to optimise the above algo, we need to do stuff in place and use advantage of the fact that the vertical nodes are already sorted
 // we create a dummy node and keep on adding stuff
-
 /*
 pseudcode for 2 such nodes with their child
 merge2(list1, list2){
@@ -38,12 +21,11 @@ merge2(list1, list2){
   }
   res next = null;
 }
-// what if list1 still has some elements left or list2
+what if list1 still has some elements left or list2
 if(list1) res->child = list1;
 same for list 2
   return dummynode->child; tc = n1+n2
   */
-// RECURSION BASED CODE
 Node* merge(Node* list1, Node* list2){
     Node* dummynode = new Node(-1);
     Node* res = dummynode;
@@ -69,7 +51,6 @@ Node* merge(Node* list1, Node* list2){
 
 Node *flatten(Node *head) {
     if(head == NULL || head->next == NULL) return head;
-    // above was our base condition
     Node* mergehead = flatten(head->next);
     head = merge(head, mergehead);
     return head;
