e04777fbc04c987bebe7d62e4e69d5ab134d1f51
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2026-03-12 18:04:27 +0530

    simpler than LL

--- DSA sheet/binary trees/hard/flatten.cpp
+++ DSA sheet/binary trees/hard/flatten.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* prev = nullptr;
@@ -24,17 +13,6 @@ public:
 
 
 // iterative approach same tc sc
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     void flatten(TreeNode* root) {
