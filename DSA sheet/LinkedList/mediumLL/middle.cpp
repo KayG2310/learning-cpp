b91ceac31672402e4673af812381ff5755b3b6a4
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2026-03-24 23:16:32 +0530

    fast slow

--- DSA sheet/LinkedList/mediumLL/middle.cpp
+++ DSA sheet/LinkedList/mediumLL/middle.cpp
@@ -1,28 +1,3 @@
-// brute force
-class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
-        int i = 1;
-        ListNode* temp = head;
-        while(temp != nullptr){
-            i++;
-            temp = temp->next;
-        }
-        temp = head;
-        int j=1;
-        while(temp != nullptr){
-            if(j==((i-1)/2)+1){
-                return temp;
-            }
-            temp = temp->next;
-            j++;
-        }
-        return temp;
-    }
-};
-
-// two pointer method
-
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
